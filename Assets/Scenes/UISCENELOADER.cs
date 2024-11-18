bff5665ff44a383a96c49010c1ab8aea9cee3b39
BrandonB2024 <brandonmb04@gmail.com>
2024-11-18 05:33:28 -0500

    menu created
    the next scene function works off of the add open scenes function in build settings so it does work

---
+++ Assets/Scenes/UISCENELOADER.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if the next scene index is valid
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes to load.");
+        }
+    }
+}
